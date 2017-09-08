28f1f2bcd5584e978d749b261b1dffaaf17ba163
Nate Denys <nate_denys@yahoo.com>
2017-09-07 22:50:23 -0700

    complete randomizing functionality

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 
 var colors = generateRandomColors(6);
-
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message")
-
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -20,13 +19,13 @@ for(var i = 0; i < squares.length; i++){
 			if(clickedColor === pickedColor){
 				messageDisplay.textContent = "Correct!";
 				changeColors(clickedColor);
+				h1.style.backgroundColor = clickedColor;
 			} else {
 				this.style.backgroundColor = "#232323";
 				messageDisplay.textContent = "Try Again"
 			}
 	});
 }
-
 
 function changeColors(color){
 	// loop through all aquares
@@ -39,4 +38,27 @@ function changeColors(color){
 function pickColor(){
 	var random = Math.floor(Math.random() * colors.length);
 	return colors[random];
+}
+
+function generateRandomColors(num){
+	// make an array
+	var arr = []
+	// repeat num times
+	for(var i = 0; i < num; i++){
+		// get random color and push into arr
+		arr.push(randomColor())
+	}
+	// return that array
+	return arr;
+}
+
+function randomColor(){
+	// pick red from 0 to 255
+	var r = Math.floor(Math.random() * 256);
+	// pick green from 0 to 255
+	var g = Math.floor(Math.random() * 256);
+	// pick blue from 0 to 255
+	var b = Math.floor(Math.random() * 256);
+	"rgb(a, g, b"
+	return "rgb(" + r + ", " + g + ", " + b + ")";
 }
